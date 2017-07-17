abcbbbb35bc7ecf87efc4574a61e5319917379a7
Kurt Wunderlich <kuwu@mac.com>
2017-07-17 17:22:27 -0400

    moved main out of GameCharacter and added gitignore file

--- src/GameCharacter.java
+++ src/GameCharacter.java
@@ -17,28 +17,28 @@ public abstract class GameCharacter {
     this.intelligence = intelligence;
   }
 
-
-  public static void main(String[] args) {
-
-
-    Warrior one = new Warrior( "Smash", 18, 3, "Sword" );
-    Warrior two = new Warrior( "Shiv", 14, 2, "Dagger" );
-    Wizard three = new Wizard( "Zap", 2, 26, 20, 28 );
-    Wizard four = new Wizard( "Splash", 4, 18, 19, 26 );
-    Wizard five = new Wizard( "Toast", 3, 22, 21, 19 );
-
-    List<GameCharacter> gameCharacters = new ArrayList<GameCharacter>();
-    gameCharacters.add( one );
-    gameCharacters.add( two );
-    gameCharacters.add( three );
-    gameCharacters.add( four );
-    gameCharacters.add( five );
-
-    for (GameCharacter game: gameCharacters) {
-          game.Play();
-    }
-
-  }
+//
+//  public static void main(String[] args) {
+//
+//
+//    Warrior one = new Warrior( "Smash", 18, 3, "Sword" );
+//    Warrior two = new Warrior( "Shiv", 14, 2, "Dagger" );
+//    Wizard three = new Wizard( "Zap", 2, 26, 20, 28 );
+//    Wizard four = new Wizard( "Splash", 4, 18, 19, 26 );
+//    Wizard five = new Wizard( "Toast", 3, 22, 21, 19 );
+//
+//    List<GameCharacter> gameCharacters = new ArrayList<GameCharacter>();
+//    gameCharacters.add( one );
+//    gameCharacters.add( two );
+//    gameCharacters.add( three );
+//    gameCharacters.add( four );
+//    gameCharacters.add( five );
+//
+//    for (GameCharacter game: gameCharacters) {
+//          game.Play();
+//    }
+//
+//  }
 
 
 
